58525212c8af411e66269015584700124e848882
Sergey Lapin <sv0lapin@gmail.com>
2019-11-26 15:19:40 +0100

    chore: lint issue

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,6 +1,9 @@
-export function remapKeys(input: {}, mapFn: (inKey: string) => string): {} {
+export function remapKeys(
+  input: { readonly [key: string]: any },
+  mapFn: (inKey: string) => string,
+): { readonly [key: string]: any } {
   return Object.entries(input).reduce(
-    (acc: {}, entry: [string, {}]): {} => ({
+    (acc: {}, entry): {} => ({
       ...acc,
       [mapFn(entry[0])]: entry[1],
     }),
